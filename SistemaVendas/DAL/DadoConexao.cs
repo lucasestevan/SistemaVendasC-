b4fab50c09b16e771d31bbebaa3c1b8bad6bb75a
lucasestevan <lucas.estevan@hotmail.com>
2019-10-12 18:27:23 -0300

    Ajustando o Modelo

--- SistemaVendas/DAL/DadoConexao.cs
+++ SistemaVendas/DAL/DadoConexao.cs
@@ -8,7 +8,9 @@ namespace DAL
         {
             get
             {
-                return "Data Source = DESKTOP-628SFJB;Initial Catalog = SistemaVenda; Integrated Security = True";
+                //return "Data Source = DESKTOP-628SFJB;Initial Catalog = SistemaVenda; Integrated Security = True";
+                return @"Data Source = DESKTOP-H923SLO\SQLEXPRESS;Initial Catalog = SistemaVenda; Integrated Security = True";
+
             }
         }
     }
